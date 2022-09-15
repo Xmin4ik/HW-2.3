d221764c9ef407981039b93abb5d9f4fd2e9025f
xmin3 <Xmin.34@gmail.com>
2022-09-15 23:00:45 +0300

    Commik 2.3.1

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,9 +1,7 @@
-public class Bicycle {
+public class Bicycle extends Car{
 
-    public String modelName;
-    public int wheelsCount;
-
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
+    public Bicycle(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
     }
+
 }
--- src/Car.java
+++ src/Car.java
@@ -1,13 +1,33 @@
 public class Car {
+    private String modelName;
+    private int wheelsCount;
+    @Override
+    public String toString() {
+        return   "модель'" + modelName + '\'' +
+                ", кол-во колес=" + wheelsCount ;
+    }
+    public String getModelName(String modelName) {
+        return modelName;
+    }
 
-    public String modelName;
-    public int wheelsCount;
-
+    public Car(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
     public void updateTyre() {
         System.out.println("Меняем покрышку");
     }
-
     public void checkEngine() {
         System.out.println("Проверяем двигатель");
     }
+
 }
